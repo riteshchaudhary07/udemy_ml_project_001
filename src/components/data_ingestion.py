1ab01681d94ef4d9a71bdf66593756fc27b6aa93
Ritesh Chaudhary <rc1134118@gmail.com>
2026-03-12 23:23:31 +0530

    Added data ingestion and data transformation pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,4 +53,4 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -4,18 +4,14 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 
-os.makedirs(logs_path, exist_ok=True)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = logs_path
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.INFO
 )
-
-
-if __name__ == "__main__":
-    logging.info("Logging has started")
